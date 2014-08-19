ae0409ffd6bf8f299861fd7e3f7efd5789ca1a17
fziche <ziche@noos.fr>
2014-08-19 11:33:43 +0200

    Test item.php

--- item.php
+++ item.php
@@ -45,6 +45,9 @@
 	<script src="js/jquery-1.10.2.js"></script>
 	<script src="js/jquery-ui-1.10.4.custom.min.js"></script>
 	<script src="js/jquery.tablesorter.min.js"></script>
+	<!-- gallery scripts -->
+	<script src="js/jquery.gallerific.js"></script>
+	<script src="js/jquery.opacityrollover.js"></script>
 	<!-- zoom scripts -->
 	<!--script type="text/javascript" src="jquery.jqzoom-core.js"></script-->
 	<!--script src="js/zoomple.js"></script-->
--- js/schliemann-item.js
+++ js/schliemann-item.js
@@ -8,12 +8,12 @@ $(function() {
 	// Initially set opacity on thumbs and add
 	// additional styling for hover effect on thumbs
 	var onMouseOutOpacity = 0.67;
-	/*$('#thumbs ul.thumbs li').opacityrollover({
+	$('#thumbs ul.thumbs li').opacityrollover({
 		mouseOutOpacity:   onMouseOutOpacity,
 		mouseOverOpacity:  1.0,
 		fadeSpeed:         'fast',
 		exemptionSelector: '.selected'
-	});*/
+	});
 	
 	// Initialize Advanced Galleriffic Gallery
 	var gallery = $('#thumbs').galleriffic({
